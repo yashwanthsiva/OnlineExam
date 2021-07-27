87788d4c67fe9135da83893a6f129e6e2b92a679
Yashwanth S <yashwanth.siva@aspiresys.com>
2021-07-27 16:01:56 +0530

    Add project files.

---
+++ OnlineExam/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineExam.Data;
+
+[assembly: HostingStartup(typeof(OnlineExam.Areas.Identity.IdentityHostingStartup))]
+namespace OnlineExam.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<OnlineExamContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("OnlineExamContextConnection")));
+
+                //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                //    .AddEntityFrameworkStores<OnlineExamContext>();
+            });
+        }
+    }
+}
